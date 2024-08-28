d63d09073b87f9a83df0ed6107e21ceddcbfa0e4
sunilsomething123 <sunilthanu789@gmail.com>
2024-08-28 22:03:27 +0530

    Update index.tsx

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -5,7 +5,11 @@ import { AuthenticationRouter } from './routers/authentication.router'
 import { BillingRouter } from './routers/billing.router'
 import { ConfigurationRouter } from './routers/configuration.router'
 import { UploadRouter } from './routers/upload.router'
+import { savedContentRouter } from './savedContent.router';
 
+export const appRouter = createRouter()
+  .merge('savedContent.', savedContentRouter)
+  // Merge other routers
 const appRouter = Trpc.mergeRouters(
   createRouter(Trpc.createRouter, Trpc.procedurePublic), // The generated tRPC router for all your models
 
@@ -16,6 +20,7 @@ const appRouter = Trpc.mergeRouters(
     billing: BillingRouter,
     configuration: ConfigurationRouter,
     upload: UploadRouter,
+    savedContent: SavedContentRouter,
   }),
 )
 
